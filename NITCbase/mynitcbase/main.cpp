fc32e92dce02b27c9684ecaff3d3cfcc27126644
nothuman <nothuman2.718@gmail.com>
2024-08-12 00:49:25 +0530

    Complete Stage 2

--- NITCbase/mynitcbase/main.cpp
+++ NITCbase/mynitcbase/main.cpp
@@ -2,12 +2,53 @@
 #include "Cache/OpenRelTable.h"
 #include "Disk_Class/Disk.h"
 #include "FrontendInterface/FrontendInterface.h"
-
-int main(int argc, char *argv[]) {
-  /* Initialize the Run Copy of Disk */
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+using namespace std;
+int main(int argc, char *argv[])
+{
   Disk disk_run;
-  // StaticBuffer buffer;
-  // OpenRelTable cache;
 
-  return FrontendInterface::handleFrontend(argc, argv);
+  // create objects for the relation catalog and attribute catalog
+  RecBuffer relCatBuffer(RELCAT_BLOCK);
+  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
+
+  //   struct HeadInfo* relCatHeader=(struct HeadInfo*)malloc(sizeof(struct HeadInfo));
+  //   struct HeadInfo* attrCatHeader=(struct HeadInfo*)malloc(sizeof(struct HeadInfo));
+
+  HeadInfo relCatHeader;
+  HeadInfo attrCatHeader;
+
+  // load the headers of both the blocks into relCatHeader and attrCatHeader.
+  // (we will implement these functions later)
+  relCatBuffer.getHeader(&relCatHeader);
+  attrCatBuffer.getHeader(&attrCatHeader);
+
+  for (int i = 0; i < relCatHeader.numEntries; i++)
+  {
+
+    Attribute relCatRecord[RELCAT_NO_ATTRS]; // will store the record from the relation catalog
+
+    relCatBuffer.getRecord(relCatRecord, i);
+
+    printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+
+    for (int j = 0; j < attrCatHeader.numEntries; j++)
+    {
+
+      // declare attrCatRecord and load the attribute catalog entry into it
+      Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+      attrCatBuffer.getRecord(attrCatRecord, j);
+      if (
+          strcmp(relCatRecord[RELCAT_REL_NAME_INDEX].sVal, attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal) == 0)
+      {
+        const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
+        printf("  %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
+      }
+    }
+    printf("\n");
+  }
+
+  return 0;
 }
