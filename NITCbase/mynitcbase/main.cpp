98836e5c7ffb7be4d195627de2e15342ff6773f4
nothuman <nothuman2.718@gmail.com>
2024-08-12 02:30:38 +0530

    Complete Stage 2 Q1

--- NITCbase/mynitcbase/main.cpp
+++ NITCbase/mynitcbase/main.cpp
@@ -4,6 +4,7 @@
 #include "FrontendInterface/FrontendInterface.h"
 #include <iostream>
 #include <stdlib.h>
+#include <vector>
 #include <string.h>
 using namespace std;
 int main(int argc, char *argv[])
@@ -25,6 +26,16 @@ int main(int argc, char *argv[])
   relCatBuffer.getHeader(&relCatHeader);
   attrCatBuffer.getHeader(&attrCatHeader);
 
+  vector<int> attrBlockLL;
+  attrBlockLL.push_back(ATTRCAT_BLOCK);
+  while (attrCatHeader.rblock != -1)
+  {
+    attrBlockLL.push_back(attrCatHeader.rblock);
+    RecBuffer temp(attrCatHeader.rblock);
+    temp.getHeader(&attrCatHeader);
+  }
+
+  // Brute force approach to print the relation and attributes
   for (int i = 0; i < relCatHeader.numEntries; i++)
   {
 
@@ -34,20 +45,26 @@ int main(int argc, char *argv[])
 
     printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
 
-    for (int j = 0; j < attrCatHeader.numEntries; j++)
+    for (int k = 0; k < attrBlockLL.size(); k++)
     {
+      RecBuffer attrCatBuffer(attrBlockLL[k]);
+      attrCatBuffer.getHeader(&attrCatHeader);
 
-      // declare attrCatRecord and load the attribute catalog entry into it
-      Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
-      attrCatBuffer.getRecord(attrCatRecord, j);
-      if (
-          strcmp(relCatRecord[RELCAT_REL_NAME_INDEX].sVal, attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal) == 0)
+      for (int j = 0; j < attrCatHeader.numEntries; j++)
       {
-        const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
-        printf("  %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
+
+        // declare attrCatRecord and load the attribute catalog entry into it
+        Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+        attrCatBuffer.getRecord(attrCatRecord, j);
+        if (
+            strcmp(relCatRecord[RELCAT_REL_NAME_INDEX].sVal, attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal) == 0)
+        {
+          const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
+          printf("  %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
+        }
       }
+      printf("\n");
     }
-    printf("\n");
   }
 
   return 0;
